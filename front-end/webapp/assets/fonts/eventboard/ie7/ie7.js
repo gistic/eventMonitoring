07ed18e26e03356539f5d313f980ba21ac119c4a
[iMomen] <coder@imomen.com>
2015-04-19 15:22:31 +0300

    Add the front page for the app.

---
+++ front-end/webapp/assets/fonts/eventboard/ie7/ie7.js
@@ -0,0 +1,38 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'eventboard\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-envelope': '&#xe600;',
+		'icon-presentation': '&#xe00e;',
+		'icon-bird': '&#xe601;',
+		'icon-circle-check': '&#xe042;',
+		'icon-linkedin': '&#xe602;',
+		'icon-twitter': '&#xe603;',
+		'icon-facebook': '&#xea8c;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
