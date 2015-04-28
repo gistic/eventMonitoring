7a607608f4af1ba93427fe0130b6dcfc24461386
Osama Hussain <shussain@gistic.org>
2015-04-28 21:10:27 +0300

    error checking uuid on adminLiveTweets and liveTweets

--- src/main/java/org/gistic/tweetboard/resources/SseResource.java
+++ src/main/java/org/gistic/tweetboard/resources/SseResource.java
@@ -10,7 +10,10 @@ import twitter4j.TwitterObjectFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 
 /**
  * Created by sohussain on 4/12/15.
@@ -21,42 +24,45 @@ public class SseResource extends EventSourceServlet {
 
     protected EventSource newEventSource(final HttpServletRequest req) {
         String uuid = req.getParameter("uuid");
-        if (uuid != null){
-            return new EventSource() {
-                private boolean connected;
 
-                public void onOpen(final Emitter emitter) throws IOException {
-                    connected = true;
-                    System.out.println("Tweets queue connection opened");
-                    while (connected) {
-                        Event e = EventMap.get(uuid);
-                        try {
-                            if (e != null) {
-                                InternalStatus status = e.getOldestTweetNotSentForApproval();//EventMonitor.getCurrentEventMonitor().tweetsApproving.getTweetToAdmin();
-                                if (status == null) {
-                                    Thread.sleep(1000);
-                                } else {
-                                    Status tweet = status.getInternalStatus();
-                                    try { emitter.data(status.getStatusString().replace("_normal", "")); }
-                                    catch (Exception ex) { break; }
-                                    //Thread.sleep(100);
-                                }
-                            } else {
-                                Thread.sleep(500);
-                            }
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    }
+        return new EventSource() {
+            private boolean connected;
+
+            public void onOpen(final Emitter emitter) throws IOException {
+                if (uuid == null || uuid.equalsIgnoreCase("undefined")) {
+                    emitter.data("wrong uuid");
                     emitter.close();
+                    return;
                 }
+                connected = true;
+                System.out.println("Tweets queue connection opened");
+                while (connected) {
+                    Event e = EventMap.get(uuid);
+                    try {
+                        if (e != null) {
+                            InternalStatus status = e.getOldestTweetNotSentForApproval();//EventMonitor.getCurrentEventMonitor().tweetsApproving.getTweetToAdmin();
+                            if (status == null) {
+                                Thread.sleep(1000);
+                            } else {
+                                Status tweet = status.getInternalStatus();
+                                try { emitter.data(status.getStatusString().replace("_normal", "")); }
+                                catch (Exception ex) { break; }
+                                //Thread.sleep(100);
+                            }
+                        } else {
+                            Thread.sleep(500);
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+                //emitter.close();
+            }
 
-                public void onClose() {
-                    System.out.println("Tweets queue connection closed");
-                    connected = false;
-                }
-            };
-            //TODO : handle errors
-        } else throw new RuntimeException();
+            public void onClose() {
+                System.out.println("Tweets queue connection closed");
+                connected = false;
+            }
+        };
     }
 }
