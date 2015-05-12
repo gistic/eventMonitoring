f10c3eb8aafef4acbf41229a0e7fa2bc288e45be
Osama Hussain <shussain@gistic.org>
2015-05-12 13:37:23 +0300

    dirty commit

--- src/main/java/org/gistic/tweetboard/resources/AdminEventSource.java
+++ src/main/java/org/gistic/tweetboard/resources/AdminEventSource.java
@@ -1,9 +1,13 @@
 package org.gistic.tweetboard.resources;
 
+import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
+import org.glassfish.jersey.media.sse.SseFeature;
 
 import javax.inject.Singleton;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 /**
  * Created by osama-hussain on 5/12/15.
@@ -14,4 +18,9 @@ public class AdminEventSource {
 
     private SseBroadcaster broadcaster = new SseBroadcaster();
 
+    @GET
+    @Produces(SseFeature.SERVER_SENT_EVENTS)
+    public EventOutput getServerSentEvents() {
+
+    }
 }
