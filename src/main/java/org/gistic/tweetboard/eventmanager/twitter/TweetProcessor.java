e3f84041c9c33d094ced11c77981199811d0ff1a
Osama Hussain <shussain@gistic.org>
2015-05-14 17:29:31 +0300

    redo logic for retweet enable-disable

--- src/main/java/org/gistic/tweetboard/eventmanager/twitter/TweetProcessor.java
+++ src/main/java/org/gistic/tweetboard/eventmanager/twitter/TweetProcessor.java
@@ -25,6 +25,7 @@ public class TweetProcessor {
     public ArrayList<String> blockedUsers = new ArrayList<String>();
     public ArrayList<String> approvedUsers = new ArrayList<String>();
     private TweetsOverTimeAnalyzer tweetsOverTimeAnalyzer = new TweetsOverTimeAnalyzer();
+    //TODO: is this even used???
     private ActivePeopleAnalyzer activePeopleAnalyzer = new ActivePeopleAnalyzer();
 
     public boolean isModerated() {
@@ -36,6 +37,16 @@ public class TweetProcessor {
     }
 
     private boolean moderated = true;
+
+    private boolean retweetEnabled = true;
+
+    public boolean isRetweetEnabled() {
+        return retweetEnabled;
+    }
+
+    public void setRetweetEnabled(boolean retweetEnabled) {
+        this.retweetEnabled = retweetEnabled;
+    }
 
     public TweetProcessor(EventBus bus, TweetDataLogic tweetDataLogic) {
         this.bus = checkNotNull(bus);
@@ -54,38 +65,94 @@ public class TweetProcessor {
     @Subscribe
     @AllowConcurrentEvents
     public void onStatusUpdate(InternalStatus status) {
-    try {
         Status tweet = status.getInternalStatus();
-        if(tweet.isRetweet()) {
+        if(tweet.isRetweet() || tweet.getText().contains("RT")) {
             tweetDataLogic.incrTotalRetweets();
         } else {
             tweetDataLogic.incrOriginalTweets();
         }
-        if (!moderated) {
-            tweetDataLogic.addToApproved(status, true);
-            return;
-        }
-        if (isBlockedUserTweet(tweet)) {
-            System.out.println("blocked user detected "
-                    + tweet.getUser().getScreenName() + ":" + tweet.getText());
-        } else if (isBadKeywordTweet(tweet)) {
-            System.out.println("bad tweet detected " + tweet.getText());
+
+        activePeopleAnalyzer.TweetArrived(tweet);
+        tweetsOverTimeAnalyzer.TweetArrived(status);
+
+        if (retweetEnabled) {
+            checkModeratedAndThen(status, tweet);
         } else {
-            tweet.getUser().getOriginalProfileImageURLHttps();
-            activePeopleAnalyzer.TweetArrived(tweet);
-            if (isApprovedUser(tweet)) {
-                tweetDataLogic.addToApproved(status, true);
+            if (tweet.isRetweet() || tweet.getText().contains("RT")) {
+                tweetDataLogic.setNewTweetMeta(status);
             } else {
-                tweetDataLogic.newArrived(status);
+                checkModeratedAndThen(status, tweet);
             }
-            tweetsOverTimeAnalyzer.TweetArrived(status);
         }
-    } catch (Exception e) { e.printStackTrace(); }
+//    try {
+//        Status tweet = status.getInternalStatus();
+//        if(tweet.isRetweet()) {
+//            tweetDataLogic.incrTotalRetweets();
+//        } else {
+//            tweetDataLogic.incrOriginalTweets();
+//        }
+//        if (!moderated) {
+//            if (!retweetEnabled && tweet.isRetweet()) {
+//                tweetDataLogic.setNewTweetMeta(status);
+//                return;
+//            }
+//            tweetDataLogic.addToApproved(status, true);
+//            return;
+//        }
+//        // if moderation in enabled, then
+//        if (isBlockedUserTweet(tweet)) {
+//            tweetDataLogic.setNewTweetMeta(status);
+//            System.out.println("blocked user detected "
+//                    + tweet.getUser().getScreenName() + ":" + tweet.getText());
+//        } else if (isBadKeywordTweet(tweet)) {
+//            tweetDataLogic.setNewTweetMeta(status);
+//            System.out.println("bad tweet detected " + tweet.getText());
+//        } else {
+//            tweet.getUser().getOriginalProfileImageURLHttps();
+//            activePeopleAnalyzer.TweetArrived(tweet);
+//            if (isApprovedUser(tweet)) {
+//                if (!retweetEnabled && tweet.isRetweet()) {
+//                    tweetDataLogic.setNewTweetMeta(status);
+//                } else {
+//                    tweetDataLogic.addToApproved(status, true);
+//                }
+//            } else {
+//                if (!retweetEnabled && tweet.isRetweet()) {
+//                    tweetDataLogic.setNewTweetMeta(status);
+//                } else {
+//                    tweetDataLogic.newArrived(status);
+//                }
+//            }
+//            tweetsOverTimeAnalyzer.TweetArrived(status);
+//        }
+//    } catch (Exception e) { e.printStackTrace(); }
         // Only save 1% of the tweets we see to the database
 //        if (random.nextInt(100) < 1) {
 //            DateTime now = new DateTime();
 //            dao.insert(now, tweet.getText());
 //        }
+    }
+
+    private void checkModeratedAndThen(InternalStatus status, Status tweet) {
+        if (moderated) {
+            if (isBlockedUserTweet(tweet) || isBadKeywordTweet(tweet)) {
+                tweetDataLogic.setNewTweetMeta(status);
+                System.out.println("blocked user detected "
+                        + tweet.getUser().getScreenName() + ":" + tweet.getText());
+                System.out.println(" OR bad tweet detected " + tweet.getText());
+            } else {
+//                tweet.getUser().getOriginalProfileImageURLHttps();
+//                    activePeopleAnalyzer.TweetArrived(tweet);
+                if (isApprovedUser(tweet)) {
+                        tweetDataLogic.addToApproved(status, true);
+                } else {
+                    tweetDataLogic.newArrived(status);
+                }
+//                    tweetsOverTimeAnalyzer.TweetArrived(status);
+            }
+        } else {
+            tweetDataLogic.addToApproved(status, true);
+        }
     }
 
     private boolean isBlockedUserTweet(Status tweet) {
