feb0fda65e000501aece837a9113acd83ecf2894
Osama Hussain <shussain@gistic.org>
2015-06-08 11:42:40 +0300

    created new api /api/twitterUsers?authToken=146937078-jCSoe4kd5ARxuOHS5KMVQLYNRXatg89gUqbca3yi

--- src/main/java/org/gistic/tweetboard/security/TwitterAuthenticator.java
+++ src/main/java/org/gistic/tweetboard/security/TwitterAuthenticator.java
@@ -3,8 +3,15 @@ package org.gistic.tweetboard.security;
 import com.google.common.base.Optional;
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
+import org.gistic.tweetboard.ConfigurationSingleton;
+import org.gistic.tweetboard.TwitterConfiguration;
 import org.gistic.tweetboard.dao.AuthDao;
 import org.gistic.tweetboard.dao.AuthDaoImpl;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Created by osama-hussain on 5/27/15.
@@ -16,7 +23,31 @@ public class TwitterAuthenticator implements Authenticator<TwitterCredentials, U
         AuthDao authDao = new AuthDaoImpl();
         if (accessToken == null) {return Optional.fromNullable(new User(true));}
         String accessTokenSecret = authDao.getAccessTokenSecret(accessToken);
-        if (accessTokenSecret != null && !accessTokenSecret.isEmpty()) return Optional.fromNullable(new User(accessToken, accessTokenSecret));
+        if (accessTokenSecret == null || accessTokenSecret.isEmpty()) return Optional.absent();
+
+        User user = new User(accessToken, accessTokenSecret);
+        if (checkUser(user)) return Optional.fromNullable(user);
+//        if (accessTokenSecret != null && !accessTokenSecret.isEmpty()) return Optional.fromNullable();
         return Optional.absent();
+    }
+
+    private boolean checkUser(User user) {
+        TwitterConfiguration twitterConfiguration = ConfigurationSingleton.
+                getInstance().getTwitterConfiguration();
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.setDebugEnabled(true);
+        builder.setOAuthConsumerKey(twitterConfiguration.getConsumerKey());
+        builder.setOAuthConsumerSecret(twitterConfiguration.getConsumerSecret());
+        builder.setOAuthAccessToken(user.getAccessToken());
+        builder.setOAuthAccessTokenSecret(user.getAccessTokenSecret());
+        Configuration configuration = builder.build();
+
+        TwitterFactory factory = new TwitterFactory(configuration);
+        try {
+            return factory.getInstance().verifyCredentials() != null;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
